ce5f0cb99fcbaec552ebdcc99501fb20d58c6d8b
andrew <andrewxhill@gmail.com>
2019-02-12 15:17:01 -0800

    rm dist folder

--- dist/Textile/migration.d.ts
+++
@@ -1,5 +0,0 @@
-export default class Migration {
-    requiresFileMigration: (repoPath: string) => Promise<boolean>;
-    runFileMigration: (repoPath: string) => Promise<void>;
-    moveTextileFiles: (repoPath: string) => Promise<void>;
-}
