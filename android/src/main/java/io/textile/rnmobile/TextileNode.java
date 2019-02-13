e54f7d3d104819ea996a068a22bb4daa303253bd
andrew <andrewxhill@gmail.com>
2019-02-13 13:25:55 -0800

    rm unused import

--- android/src/main/java/io/textile/rnmobile/TextileNode.java
+++ android/src/main/java/io/textile/rnmobile/TextileNode.java
@@ -13,7 +13,6 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import org.json.JSONObject;
 
-import java.text.MessageFormat;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
