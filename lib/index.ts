d8bec78b630c41d9210960b414e06dd590c41800
Aaron Sutula <aaron.sutula@gmail.com>
2019-02-21 16:09:06 -0600

    Export utility functions, one for converting from Timestamp for now

--- lib/index.ts
+++ lib/index.ts
@@ -3,8 +3,9 @@ export { default as CameraRoll } from './CameraRoll'
 import * as API from './Textile/API'
 import Events, { TextileEvents } from './Textile/events'
 import Textile, { BackgroundTask } from './Textile'
+import * as util from './util'
 
-export { Textile, API, Events, BackgroundTask, TextileEvents }
+export { Textile, API, Events, BackgroundTask, TextileEvents, util }
 
 export * from './Textile/Models'
 
