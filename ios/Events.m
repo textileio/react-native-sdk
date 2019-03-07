02e6e9ab780e96af73572b1a7b8d7d201679a46b
Aaron Sutula <hi@asutula.com>
2019-03-07 15:18:47 -0600

    Handle events with no data (#42)
    * Handle events with no data
    
    * Real fix, just sends base64 strings

--- ios/Events.m
+++ ios/Events.m
@@ -43,16 +43,16 @@ RCT_EXPORT_MODULE();
 {
   NSArray *eventDetails = [notification.userInfo valueForKey:@"detail"];
   NSString *eventName = [eventDetails objectAtIndex:0];
-  NSDictionary *eventData = [eventDetails objectAtIndex:1];
-
+  id eventData;
+  if (eventDetails.count > 1) {
+    eventData = [eventDetails objectAtIndex:1];
+  }
   [self sendEventWithName:eventName body:eventData];
 }
 
-+ (void)emitEventWithName:(NSString *)name andPayload:(NSString *)payload
++ (void)emitEventWithName:(NSString *)name andPayload:(NSString *)base64string
 {
-  NSData *data = [payload dataUsingEncoding:NSUTF8StringEncoding];
-  NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-  NSDictionary *eventDetail = @{@"detail":@[name,json]};
+  NSDictionary *eventDetail = @{@"detail": base64string ? @[name, base64string] : @[name]};
   [[NSNotificationCenter defaultCenter] postNotificationName:@"event-emitted"
                                                       object:self
                                                     userInfo:eventDetail];
