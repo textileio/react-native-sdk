45f55137b9415cb6d4867e1f5977e78a5a7fdd3c
Andrew W. Hill <andrew@textile.io>
2019-03-08 11:07:26 +0100

    Adds dataAtPath method to API (#46)

--- lib/Textile/API/ipfs.ts
+++ lib/Textile/API/ipfs.ts
@@ -2,6 +2,11 @@ import { NativeModules } from 'react-native'
 
 const { TextileNode } = NativeModules
 
+export async function dataAtPath(path: string): Promise<string> {
+  const result = await TextileNode.dataAtPath(path)
+  return result as string
+}
+
 export async function peerId(): Promise<string> {
   const result = await TextileNode.peerId()
   return result as string
