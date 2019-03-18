0e75879e619379d9b57f3646d6d6e16516b0f82d
andrew <andrewxhill@gmail.com>
2019-03-17 20:15:43 -0700

    adds util back

---
+++ lib/Textile/util.ts
@@ -0,0 +1,11 @@
+import { pb } from './Models'
+
+export function timestampToDate(timestamp?: pb.google.protobuf.ITimestamp) {
+  let millis: number
+  if (!timestamp) {
+    millis = 0
+  } else {
+    millis = timestamp.seconds as number * 1e3 + timestamp.nanos / 1e6
+  }
+  return new Date(millis)
+}
