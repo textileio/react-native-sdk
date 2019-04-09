a94964753f4e9bdb0e5ee7b8a0e082b9db0ad048
Aaron Sutula <aaron.sutula@gmail.com>
2019-04-09 17:55:02 -0500

    Skeleton bridge class

---
+++ ios/TextileBridge.m
@@ -0,0 +1,25 @@
+#import "TextileBridge.h"
+
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridge.h>
+#elif __has_include(“RCTBridge.h”)
+#import “RCTBridge.h”
+#else
+#import “React/RCTBridge.h”
+#endif
+
+#define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
+@implementation TextileBridge
+
+RCT_EXPORT_MODULE();
+
+@end
+
+@implementation RCTBridge (TextileBridge)
+
+- (TextileBridge *)textileBridge {
+  return [self moduleForClass:[TextileBridge class]];
+}
+
+@end
