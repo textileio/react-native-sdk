c237c7ec2bec202eead772823a3d646a39682e24
Aaron Sutula <aaron.sutula@gmail.com>
2019-04-10 18:47:36 -0500

    Textile api + event handler/emitter

--- ios/TextileBridge.m
+++ ios/TextileBridge.m
@@ -9,6 +9,7 @@
 #endif
 
 #import <Textile/TextileApi.h>
+#import "TextileHandler.h"
 #import "utils.h"
 
 #define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
@@ -16,6 +17,33 @@
 @implementation TextileBridge
 
 RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(initialize:(BOOL)debug logToDisk:(BOOL)logToDisk resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+  NSError *error;
+  NSString *phrase = [Textile initializeWithDebug:debug logToDisk:logToDisk error:&error];
+  if (error) {
+    fulfillWithResult(nil, error, resolve, reject);
+  } else {
+    Textile.instance.delegate = [[TextileHandler alloc] init];
+    fulfillWithResultAndNilDefault(phrase, @"", error, resolve, reject);
+  }
+}
+
+RCT_EXPORT_METHOD(version:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+  NSString *result = Textile.instance.version;
+  fulfillWithResult(result, nil, resolve, reject);
+}
+
+RCT_EXPORT_METHOD(gitSummary:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+  NSString *result = Textile.instance.gitSummary;
+  fulfillWithResult(result, nil, resolve, reject);
+}
+
+RCT_EXPORT_METHOD(summary:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+  NSError *error;
+  Summary *summary = [Textile.instance summary:&error];
+  fulfillWithResult([summary.data base64EncodedStringWithOptions:0], error, resolve, reject);
+}
 
 @end
 
---
+++ ios/TextileHandler.h
@@ -0,0 +1,17 @@
+//
+//  TextileHandler.h
+//  textile-react-native-sdk
+//
+//  Created by Aaron Sutula on 4/10/19.
+//
+
+#import <Foundation/Foundation.h>
+#import <Textile/TextileDelegate.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TextileHandler : NSObject <TextileDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
