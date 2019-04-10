a472c9203c1127de91d82033621e20c27d8937a9
Aaron Sutula <aaron.sutula@gmail.com>
2019-04-10 14:07:18 -0500

    Flags api

---
+++ ios/FlagsBridge.m
@@ -0,0 +1,33 @@
+#import "FlagsBridge.h"
+
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridge.h>
+#elif __has_include(“RCTBridge.h”)
+#import “RCTBridge.h”
+#else
+#import “React/RCTBridge.h”
+#endif
+
+#import <Textile/TextileApi.h>
+#import "utils.h"
+
+#define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
+@implementation FlagsBridge
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(add:(NSString*)blockId resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+  NSError *error;
+  fulfillWithResult([Textile.instance.flags add:blockId error:&error], error, resolve, reject);
+}
+
+@end
+
+@implementation RCTBridge (FlagsBridge)
+
+- (FlagsBridge *)flagsBridge {
+  return [self moduleForClass:[FlagsBridge class]];
+}
+
+@end
