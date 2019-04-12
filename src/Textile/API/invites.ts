56d736c9560f6fac7df96e0146f333239ddf56c7
Aaron Sutula <aaron.sutula@gmail.com>
2019-04-12 16:19:07 -0500

    Updated invited module

--- src/Textile/API/invites.ts
+++ src/Textile/API/invites.ts
@@ -1,38 +1,41 @@
 import { NativeModules } from 'react-native'
 import { Buffer } from 'buffer'
-import { pb } from '../Models'
 
-const { TextileNode } = NativeModules
+import {
+  IExternalInvite,
+  ExternalInvite,
+} from './model'
+
+const { InvitesBridge } = NativeModules
 
 /**
  * Add a new Thread invite by invitee address.
  * ```typescript
- * API.invites.add(threadId, address);
+ * Textile.invites.add(threadId, address);
  * ```
  */
-export async function add(threadId: string, address: string): Promise<string> {
-  const result = await TextileNode.addInvite(threadId, address)
-  return result as string
+export async function add(threadId: string, address: string): Promise<void> {
+  return InvitesBridge.add(threadId, address)
 }
 
 /**
  * Add an external Thread invite, returning a sharable object.
  * ```typescript
- * API.invites.addExternal(threadId);
+ * Textile.invites.addExternal(threadId);
  * ```
  */
-export async function addExternal(threadId: string): Promise<pb.IExternalInvite> {
-  const result = await TextileNode.addExternalInvite(threadId)
-  return pb.ExternalInvite.decode(Buffer.from(result, 'base64'))
+export async function addExternal(threadId: string): Promise<IExternalInvite> {
+  const result = await InvitesBridge.addExternal(threadId)
+  return ExternalInvite.decode(Buffer.from(result, 'base64'))
 }
 
 /**
  * Accept an external invite.
  * ```typescript
- * API.invites.acceptExternal(id, key);
+ * Textile.invites.acceptExternal(id, key);
  * ```
  */
 export async function acceptExternal(id_: string, key: string): Promise<string> {
-  const result = await TextileNode.acceptExternalInvite(id_, key)
+  const result = await InvitesBridge.acceptExternal(id_, key)
   return result as string
 }
