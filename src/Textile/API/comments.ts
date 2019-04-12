699e7c5dfcbd45aaa29ab8ee813463529f0d170b
Aaron Sutula <aaron.sutula@gmail.com>
2019-04-12 15:12:01 -0500

    Updated comments module

--- src/Textile/API/comments.ts
+++ src/Textile/API/comments.ts
@@ -1,14 +1,14 @@
 import { NativeModules } from 'react-native'
 
-const { TextileNode } = NativeModules
+const { CommentsBridge } = NativeModules
 
 /**
  * Add a new comment to a Thread by blockId.
  * ```typescript
- * API.comments.add(blockId, body);
+ * Textile.comments.add(blockId, body);
  * ```
  */
 export async function add(blockId: string, body: string): Promise<string> {
-  const result = await TextileNode.addComment(blockId, body)
+  const result = await CommentsBridge.add(blockId, body)
   return result as string
 }
