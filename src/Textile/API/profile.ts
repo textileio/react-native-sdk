e1640b9809e75351b1f3b6e6fbe919b0db177586
Aaron Sutula <aaron.sutula@gmail.com>
2019-04-12 16:38:31 -0500

    updated profile module

--- src/Textile/API/profile.ts
+++ src/Textile/API/profile.ts
@@ -1,58 +1,62 @@
 import { NativeModules } from 'react-native'
 import { Buffer } from 'buffer'
-import { pb } from '../Models'
 
-const { TextileNode } = NativeModules
+import {
+  IPeer,
+  Peer,
+} from './model'
+
+const { ProfileBridge } = NativeModules
 
 /**
  * Get the profile.
  * ```typescript
- * API.profile.get();
+ * Textile.profile.get();
  * ```
  */
-export async function get(): Promise<pb.IPeer> {
-  const result = await TextileNode.profile()
-  return pb.Peer.decode(Buffer.from(result, 'base64'))
+export async function get(): Promise<IPeer> {
+  const result = await ProfileBridge.get()
+  return Peer.decode(Buffer.from(result, 'base64'))
 }
 
 /**
  * Get the name.
  * ```typescript
- * API.profile.name();
+ * Textile.profile.name();
  * ```
  */
 export async function name(): Promise<string | undefined> {
-  const result: string = await TextileNode.name()
+  const result: string = await ProfileBridge.name()
   return result.length > 0 ? result : undefined
 }
 
 /**
  * Update the name.
  * ```typescript
- * API.profile.setName(name);
+ * Textile.profile.setName(name);
  * ```
  */
 export async function setName(name: string): Promise<void> {
-  await TextileNode.setName(name)
+  await ProfileBridge.setName(name)
 }
 
 /**
  * Get the BlockId of the current user Avatar.
  * ```typescript
- * API.profile.avatar();
+ * Textile.profile.avatar();
  * ```
  */
 export async function avatar(): Promise<string | undefined> {
-  const result: string = await TextileNode.avatar()
+  const result: string = await ProfileBridge.avatar()
   return result.length > 0 ? result : undefined
 }
 
 /**
  * Set a new Avatar by ID.
  * ```typescript
- * API.profile.setAvatar(id);
+ * Textile.profile.setAvatar(id);
  * ```
  */
 export async function setAvatar(id_: string): Promise<void> {
-  await TextileNode.setAvatar(id_)
+  await ProfileBridge.setAvatar(id_)
 }
