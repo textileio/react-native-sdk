bbb4802753847ce8bc574a4ee0abac8a06f6f8d9
Aaron Sutula <aaron.sutula@gmail.com>
2019-04-12 15:28:55 -0500

    updated contacts api

--- src/Textile/API/contacts.ts
+++ src/Textile/API/contacts.ts
@@ -1,73 +1,85 @@
 import { NativeModules } from 'react-native'
 import { Buffer } from 'buffer'
-import { pb } from '../Models'
 
-const { TextileNode } = NativeModules
+import {
+  IContact,
+  Contact,
+  IContactList,
+  ContactList,
+  IThreadList,
+  ThreadList,
+  IContactQuery,
+  ContactQuery,
+  IQueryOptions,
+  QueryOptions,
+} from './model'
+
+const { ContactsBridge } = NativeModules
 
 /**
  * Add a new Contact after fetching the Contact information.
  */
-export async function add(contact: pb.IContact): Promise<void> {
-  const payload = pb.Contact.encode(contact).finish()
-  await TextileNode.addContact(Buffer.from(payload).toString('base64'))
+export async function add(contact: IContact): Promise<void> {
+  const payload = Contact.encode(contact).finish()
+  await ContactsBridge.add(Buffer.from(payload).toString('base64'))
 }
 
 /**
  * Get Contact information by address.
  */
-export async function get(address: string): Promise<pb.IContact | undefined> {
-  const result = await TextileNode.contact(address)
-  if (!result) {
+export async function get(address: string): Promise<IContact | undefined> {
+  const result: string = await ContactsBridge.get(address)
+  if (result.length === 0) {
     return undefined
   }
-  return pb.Contact.decode(Buffer.from(result, 'base64'))
+  return Contact.decode(Buffer.from(result, 'base64'))
 }
 
 /**
  * List all known Contacts.
  */
-export async function list(): Promise<pb.IContactList> {
-  const result = await TextileNode.contacts()
-  return pb.ContactList.decode(Buffer.from(result, 'base64'))
+export async function list(): Promise<IContactList> {
+  const result = await ContactsBridge.list()
+  return ContactList.decode(Buffer.from(result, 'base64'))
 }
 
 /**
  * Remove a Contact by their address.
  */
 export async function remove(address: string): Promise<void> {
-  return TextileNode.removeContact(address)
+  return ContactsBridge.remove(address)
 }
 
 /**
  * List all Threads in common with a Contact.
  * ```typescript
- * API.contacts.threads(address);
+ * Textile.contacts.threads(address);
  * ```
  */
-export async function threads(address: string): Promise<pb.IThreadList> {
-  const result = await TextileNode.contactThreads(address)
-  return pb.ThreadList.decode(Buffer.from(result, 'base64'))
+export async function threads(address: string): Promise<IThreadList> {
+  const result = await ContactsBridge.threads(address)
+  return ThreadList.decode(Buffer.from(result, 'base64'))
 }
 
 /**
  * Search for Contacts over network.
  * ```typescript
- * API.contacts.search(query, options);
+ * Textile.contacts.search(query, options);
  * ```
  */
-export async function search(query: pb.IContactQuery, options: pb.IQueryOptions): Promise<string> {
-  return TextileNode.searchContacts(
-    Buffer.from(pb.ContactQuery.encode(query).finish()).toString('base64'),
-    Buffer.from(pb.QueryOptions.encode(options).finish()).toString('base64'),
+export async function search(query: IContactQuery, options: IQueryOptions): Promise<void> {
+  return ContactsBridge.search(
+    Buffer.from(ContactQuery.encode(query).finish()).toString('base64'),
+    Buffer.from(QueryOptions.encode(options).finish()).toString('base64'),
   )
 }
 
 /**
  * Cancel an ongoing contact search.
  * ```typescript
- * API.contacts.cancelSearch();
+ * Textile.contacts.cancelSearch();
  * ```
  */
 export async function cancelSearch(): Promise<void> {
-  return TextileNode.cancelSearch()
+  return ContactsBridge.cancelSearch()
 }
